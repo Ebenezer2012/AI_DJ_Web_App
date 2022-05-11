8e9a98f3ea7e150755ddec35a128ad98ee4d61c0
Ebenezer2012 <89516748+Ebenezer2012@users.noreply.github.com>
2022-05-11 17:46:17 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ rightWristY = 0;
 
 function preload()
 {
-    song = loadSound("music.mp3");
+    song = loadSound('music.mp3')
 }
 
 function setup() 
@@ -31,6 +31,8 @@ function gotPoses(results)
    if(results.length > 0)
    {
     console.log(results);
+    scoreLeftWrist = results[0].pose.keypoints[9].score;
+    console.log("scoreLeftWrist = " + scoreLeftWrist);
     leftWristX = results[0].pose.leftWrist.x;
     leftWristY = results[0].pose.leftWrist.y;
     console.log("leftWristX =" + leftWristX +"leftWristY ="+ leftWristY );
@@ -46,7 +48,20 @@ function gotPoses(results)
 
 function draw()
 {
-Image(video, 0,0,600,500);
+image(video, 0,0,600,500);
+fill("#FF0000");
+stroke("#FF0000");
+
+if(scoreLeftWrist > 0.2)
+{
+    circle(leftWristX,leftWristY,20);
+    InNumberleftWristY = Number(leftWristY);
+    remove_decimals = floor(InNumberleftWristY);
+    volume = remove_decimals/500;
+    document.getElementById("volume").innerHTML = "Volume =" + volume;
+    song.setVolume(volume);
+}
+
 }
 
 function play()
